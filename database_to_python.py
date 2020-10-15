fe2ec51430b60ba27add5e04d906ba87ada4c530
Gnome17530 <17530@burnside.school.nz>
2020-10-16 11:00:01 +1300

    Second changes stage

--- database_to_python.py
+++ database_to_python.py
@@ -1,4 +1,15 @@
 ''' A simple program to put a database into python by Naomi 12.10.2020'''
 
+'Imports'
+import sqlite3
+
 'Function'
-def
+
+DATABASE_FILE = "discgolf.db"
+
+with sqlite3.connect(DATABASE_FILE) as connection:
+    cursor = connection.cursor()
+    sql = "SELECT * FROM category"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results)
